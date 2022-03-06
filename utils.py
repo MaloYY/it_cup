aad3f6724ff5fb13261c770c605f261c8ce8ce0f
malov_pavel <kiba.malov@gmail.com>
2022-03-06 21:47:38 +0300

    add util to save config

--- utils.py
+++ utils.py
@@ -1,7 +1,17 @@
+import json
+
 def fine_print(scores):
     print("m_acc:    ", scores['mean_test_accuracy'])
     print("m_f1:     ", scores['mean_test_f1'])
     print("m_rauc:   ", scores['mean_test_roc_auc'])
     print("m_prec:   ", scores['mean_test_precision'])
     print("m_recall: ", scores['mean_test_recall'])
+
+
+def save_config(id, config, model):
+    with open('log_config.txt', 'w') as outfile:
+        outfile.write(f"ID: p{id}  MODEL: {model}\n")
+        json.dump(config, outfile)
+        outfile.write('\n================\n\n')
+
     
