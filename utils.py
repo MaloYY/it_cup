b80f2435a4d62d8827ca2745e0c007ead5e67f6b
malov_pavel <kiba.malov@gmail.com>
2022-03-06 19:56:23 +0300

    add fine_print

--- utils.py
+++ utils.py
@@ -1,2 +1,7 @@
 def fine_print(scores):
-    pass
+    print("m_acc:    ", scores['mean_test_accuracy'])
+    print("m_f1:     ", scores['mean_test_f1'])
+    print("m_rauc:   ", scores['mean_test_roc_auc'])
+    print("m_prec:   ", scores['mean_test_precision'])
+    print("m_recall: ", scores['mean_test_recall'])
+    
